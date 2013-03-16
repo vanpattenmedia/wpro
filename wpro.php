fa1a8f24d01ce6647b54a0e3a481e3fff35e43fc
Eugen Istoc <eugenistoc@gmail.com>
2013-03-16 03:13:51 -0400

    Amazon S3 url update

--- wpro.php
+++ wpro.php
@@ -509,7 +509,7 @@ class WordpressReadOnly extends WordpressReadOnlyGeneric {
 			if (wpro_get_option('wpro-aws-virthost')) {
 				$data['baseurl'] = 'http://' . trim(str_replace('//', '/', wpro_get_option('wpro-aws-bucket') . '/' . trim(wpro_get_option('wpro-folder'))), '/');
 			} else {
-				$data['baseurl'] = 'http://s3.amazonaws.com/' . trim(str_replace('//', '/', get_option('wpro-aws-bucket') . '/' . trim(get_option('wpro-folder'))), '/');
+				$data['baseurl'] = 'http://s3.amazonaws.com/' . trim(str_replace('//', '/', wpro_get_option('wpro-aws-bucket') .'/'. trim(wpro_get_option('wpro-folder'))), '/');
 			}
 		}
 		$data['path'] = $this->upload_basedir . $data['subdir'];
